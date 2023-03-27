6d46bd89ae451099562604d3121d181ad674364d
wxw <wuxw@mail.ustc.edu.cn>
2023-03-27 15:33:13 +0800

    add help, q cmd

--- main.c
+++ main.c
@@ -1,5 +1,15 @@
-#include <stio.h>
+#include <stdio.h>
+#include <string.h>
 
 int main(){
-  printf("hello, world\n");
+  scanf("%s", cmd);
+  if(strcmp(cmd, "help") == 0){
+    printf("this is help cmd\n");
+  }
+  else if(strcmp(cmd, "q") == 0){
+    return 0;
+  }
+  else{
+    printf("wrong command\n");
+  }
 }
